2d0118fec54add07e3b5d543951ab0a035174fa3
alan00211 <138538323+alan00211@users.noreply.github.com>
2024-04-20 23:06:27 -0300

    ok

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function toggleText(element) {
+    const servicioTexto = element.nextElementSibling;
+    servicioTexto.style.display = servicioTexto.style.display === 'none' ? 'block' : 'none';
+  }
