4d47882da7db8fac2c08ef96d80140b9504c2996
alan00211 <138538323+alan00211@users.noreply.github.com>
2024-04-21 17:14:53 -0300

    final

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ function toggleText(element) {
     const servicioTexto = element.nextElementSibling;
     servicioTexto.style.display = servicioTexto.style.display === 'none' ? 'block' : 'none';
   }
+
+
+  const btnsLeerMas = document.querySelectorAll('.btn-primary');
+  btnsLeerMas.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const cardBody = btn.parentNode;
+      const cardParagraph = cardBody.querySelector('.card-paragraph');
+      cardParagraph.style.display = 'block';
+    });
+  });
